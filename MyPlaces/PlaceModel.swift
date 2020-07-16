2137b4fb12fa512a9a7e3fd4963b8cee6f0114a1
Mugaev <mugaevis@gmail.com>
2020-07-16 22:46:13 +0300

    08. Work with Model

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,37 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Admin on 16.07.2020.
+//  Copyright © 2020 Ilnur Mugaev. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+           "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+           "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+           "Speak Easy", "Morris Pub", "Вкусные истории",
+           "Классик", "Love&Life", "Шок", "Бочка"
+       ]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Казань", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+    
+}
+
